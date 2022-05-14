a4df1450bb09a3ca0fb5e2968ee6f4bc6f4d418e
Lam Dinh <lamdinhreply@gmail.com>
2022-05-14 09:40:44 +0200

    User name function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+function formatName(user) {
+    console.log(user);
+    return user.firstName + ' ' + user.lastName;
+}
+
+    const user = {
+        firstName:  'Lam',
+        lastName : 'Dinh',
+    };
+
+    const element = (
+        <h1>
+            Hello, {formatName(user)}!
+        </h1>
+    );
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<h1>{element}</h1>);

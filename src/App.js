763a8281f8bc0ad8cc712fe3de8ab68821568399
Lam Dinh <lamdinhreply@gmail.com>
2023-04-14 14:47:27 +0200

    Deleted handleClick from Square component & added to Board component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,46 @@
 import { useState } from "react";
 
-function Square() {
-
-  //value: store the current value of the Square in state, 
-  //setValue: and change it when the Square is clicked.
-  const [value, setValue] = useState(null);
-
-  function handleClick(){
-    //change Square value on click
-    setValue('X');
-  }
-
+function Square({value, onSquareClick}) {
   return (
     <button 
-      className="square" 
-      onClick={handleClick}
-    >
-      {value}
+      className="square"
+      onClick={onSquareClick}
+    > 
+      {value} 
     </button>
   )
 }
 
 export default function Board() {
+  //squares: state variable
+  //Array(0).fill(null): create an array with 9 elements and set them to null;
+  //Each entry in the array corresponds to the value of a square 
+  const [squares, setSquares] = useState(Array(9).fill(null)); 
+  function handleClick(){
+    const nextSquares = squares.slice();
+    nextSquares[0] = "X";
+    setSquares(nextSquares);
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={handleClick}/>
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
-  )
+)
 }
 
 

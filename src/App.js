7cac5f5d737e0dacb63bab58082a51fc18b68150
Lam Dinh <lamdinhreply@gmail.com>
2023-04-14 15:08:38 +0200

    Prevented overwriting the same square

--- src/App.js
+++ src/App.js
@@ -12,14 +12,25 @@ function Square({value, onSquareClick}) {
 }
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   //squares: state variable
   //Array(0).fill(null): create an array with 9 elements and set them to null;
   //Each entry in the array corresponds to the value of a square 
   const [squares, setSquares] = useState(Array(9).fill(null)); 
   function handleClick(i){
+    //prevent overwriting the same square
+    if(squares[i]){
+      return;
+    }
+
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    if(xIsNext) {
+      nextSquares[i] = 'X';
+    }else{
+      nextSquares[i] = 'O'
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (

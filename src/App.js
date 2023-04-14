002bc7fcb0725a4476d9d5c5c1188cff9af4a5d1
Lam Dinh <lamdinhreply@gmail.com>
2023-04-14 15:27:13 +0200

    Created function to calculate winner. Added status of the game.

--- src/App.js
+++ src/App.js
@@ -17,6 +17,14 @@ export default function Board() {
   //Array(0).fill(null): create an array with 9 elements and set them to null;
   //Each entry in the array corresponds to the value of a square 
   const [squares, setSquares] = useState(Array(9).fill(null)); 
+  
+  const winner = calculateWinner(squares);
+  let status;
+  if(winner) {
+    status = "The winner is:" + winner;
+  }else {
+    status = "Next player is:" + (xIsNext ? 'X' : 'O');
+  }
   function handleClick(i){
     //prevent overwriting the same square
     if(squares[i]){
@@ -35,6 +43,7 @@ export default function Board() {
 
   return (
     <>
+      <div className="status"> {status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() =>handleClick(0)}/>
         <Square value={squares[1]} onSquareClick={() =>handleClick(1)} />
@@ -51,7 +60,25 @@ export default function Board() {
         <Square value={squares[8]} onSquareClick={() =>handleClick(8)} />
       </div>
     </>
-)
+  )
 }
 
-
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
